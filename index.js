79befb95126e6255c07183cf8277ea1d30f91bde
Ikehviky <victorikeh77@gmail.com>
2021-12-20 06:37:52 +0100

    crud with mongoose

--- index.js
+++ index.js
@@ -1,38 +1,123 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('Connected to MogoDB...'))
-    .catch(err => console.error('Could not connect to MogoDB', err))
+    .then(() => console.log('Connecting to database...'))
+    .catch(err => console.log('Error:', err.message));
 
-const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    data: { type:Date, default: Date.now },
-    isPublished: Boolean
-});
+    const createSchema = mongoose.Schema({
+        title: {
+            type: String,
+            required: true,
+            minlength: 5, 
+            maxlength: 255
+        },
+        category:{
+            type: String,
+            required: true,
+            // enum: ['web', 'mobile', 'cloud'],
+            lowercase: true
+        }, 
+        author: String,
+        tag: {
+            isAsync: true,
+            type: Array,
+            validate: {
+                validator: function(v){
+                    setTimeout(() => {
+                        const result = v && v.length > 0;
+                        // callback(result);
+                    },4000)
+                },
+                message: 'A course shoud have at least one tag.'
+            }
+        },
+        date: {type: Date, default: Date.now},
+        isPublished: Boolean,
+        price: {
+            type: Number, 
+            required: function (){ return this.isPublished; },
+            min: 10,
+            max: 20 
+        }
+    });
+    
+const Course = mongoose.model('course', createSchema);
 
-const Course = mongoose.model('Course', courseSchema)
-
-async function createCourse() {
+async function createCourse(){
     const course = new Course({
-        name: 'Angular course',
-        author: 'victor',
-        tags: ['Angular', 'frontEnd'],
-        isPublished: true
+        title: 'angular course',
+        author: 'Ikeh Victor',
+        category: 'Web',
+        tag: ['frontend'],
+        isPublished: true,
+        price: 20
     })
     
-    const result = await course.save();
-    console.log(result);
-};
+    try{
+        const result = await course.save();
+        console.log(result);
+    }catch(ex){
+        console.log(ex.message);
+    }
+}
+
+// createCourse();
 
 async function getCourses(){
-    const courses = await Course
-        .find({ author:'victor', isPublished:true })
-        .limit(10)
-        .sort({name: -1})
-        .select({name:1, tags:1});
+    // const pageNumber = 2;//pagination
+    // const pageSize = 10;
+
+    const courses = Course
+        .find({ author: "Mosh", isPublished: true })
+        // .skip((pageNumber - 1) * pageSize)
+        // .limit(pageSize)
+        .sort({ name: 1 })
+        .select({ name:1, tags: 1 });
     console.log(courses);
 }
 
-getCourses();
+
+// updating a document by finding by id
+async function updateCourse(id){
+    const course = await Course.findById(id);
+    if(!course) return "This course does not exist";
+   course.set({
+       isPublished: true,
+       author: 'Another Author', 
+   })
+
+   const result = await course.save();
+   console.log(result);
+}
+
+
+// updating a document direct in mongodb compass
+// async function updateCourse(id){
+//     const result = await Course.update({_id: id}, {
+//         author: 'Mosh',
+//         isPublished: true
+//     })
+   
+//    console.log(result);
+// }
+
+
+//finding and updating at once and also sowing updated id
+// async function updateCourse(id){
+//     const result = await Course.findByIdAndUpdate(id, {
+//       $set: {
+//         author: 'ikeh vasdafsd',
+//         isPublished: true
+//       }
+//     }, {new: true})
+   
+//    console.log(result);
+// }
+
+async function removeCourse(id){
+   const result = await Course.deleteOne({_id: id});
+//    const result = await Course.deleteOne(id);
+   console.log(result);
+}
+
+// removeCourse("61bd452bbe51fe774e28c87c");
